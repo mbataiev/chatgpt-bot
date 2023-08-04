29a304c6566ffef4731239ae303e9d17242b9ee2
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-08-04 22:08:50 +0300

    added controller

---
+++ src/main/java/com/app/bot/chatgptbot/controller/BotController.java
@@ -0,0 +1,33 @@
+package com.app.bot.chatgptbot.controller;
+
+import com.app.bot.chatgptbot.dto.ChatGptRequest;
+import com.app.bot.chatgptbot.dto.ChatGptResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/bot")
+public class BotController {
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt){
+        ChatGptRequest request=new ChatGptRequest(model, prompt);
+        ChatGptResponse chatGptResponse = template.postForObject(apiURL, request, ChatGptResponse.class);
+        return chatGptResponse.getChoices().get(0).getMessage().getContent();
+    }
+}
