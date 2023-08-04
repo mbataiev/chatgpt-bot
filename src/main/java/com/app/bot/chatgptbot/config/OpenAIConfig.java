d532221563efb66c755b459f77a95f5533333a0d
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-08-04 22:14:08 +0300

    added controller. Closes #1

--- src/main/java/com/app/bot/chatgptbot/config/OpenAIConfig.java
+++ src/main/java/com/app/bot/chatgptbot/config/OpenAIConfig.java
@@ -1,6 +1,5 @@
 package com.app.bot.chatgptbot.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,13 +7,11 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class OpenAIConfig {
 
-    @Value("${openai.api.key}")
-    String openaiApiKey;
-
     @Bean
     public RestTemplate template(){
         RestTemplate restTemplate=new RestTemplate();
         restTemplate.getInterceptors().add((request, body, execution) -> {
+            String openaiApiKey = System.getenv("OPENAI_API_KEY");
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
             return execution.execute(request, body);
         });
